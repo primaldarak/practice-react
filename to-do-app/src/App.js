fc6eede386574aca3b2a1a73973c52c0d9e3ecf3
Vladyslav Zaikovskyi <vladyslav.zaikovskyi@gmail.com>
2022-10-19 13:18:04 +0300

    Todo app submit

---
+++ to-do-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ToDoList from "./components/ToDoList";
+
+function App() {
+    return (
+        <div className="todo-app">
+            <ToDoList />
+        </div>
+    );
+}
+
+export default App;
